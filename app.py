a1757495911a995e915a18c1308424d2e4e08d72
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2022-11-09 21:41:47 -0600

    rename app, cleanup variable names

--- app.py
+++ app.py
@@ -5,18 +5,17 @@ from data_model import db
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template(
-        "welcome.html",
-        message="Here's a message from the view that is loaded via Jinja"
-        )
+# def home():
+#     return render_template(
+#         "welcome.html",
+#         message="Here's a message from the view that is loaded via Jinja"
+#         )
 
-@app.route("/superheroes")
 def superhero_view():
-    superheroes = db[0]
+    data = db[0]
     return render_template(
         "superheroes.html", 
-        hero=superheroes
+        superhero=data
         )
 
 if __name__ == '__main__':
